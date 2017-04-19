242b283bd8ec60320eb7677305d36c1c23bf05ed
Ayoub Ben Achour <ayoub.benachour@esprit.tn>
2017-04-19 12:58:24 +0100

    Update reviews.controller.js

--- public/admin/controllers/reviews.controller.js
+++ public/admin/controllers/reviews.controller.js
@@ -1,10 +1,26 @@
 (function (angular) {
     'use strict';
-    function ControllerFN($scope,Review,Session,$state) {
+    function ControllerFN($scope,User,Scenario,Review,Session,$state) {
+        $scope.addreview = {};
+        $scope.aditreview = {};
+        
         $scope.reviews = [];
+        $scope.users = [];
+        $scope.scenarios = [];
+
 
         Review.query().$promise.then((data)=>{
             $scope.reviews = data;
+            console.log(data);
+
+        });
+        
+        User.query().$promise.then((data)=>{
+            $scope.users = data;
+        });
+
+        Scenario.query().$promise.then((data)=>{
+            $scope.scenarios = data;
         });
 
         $scope.remove = (id)=>{
@@ -14,9 +30,35 @@
                 $scope.reviews.splice(index,1);
             });
         }
+        
+        $scope.add = function(){
+            var temp = new Review($scope.addreview);
+            temp.$save().then((data)=>{
+                data.user = $scope.users[($scope.users.map(x=>x._id).indexOf(data.user))];
+                data.scenario = $scope.scenarios[($scope.scenarios.map(x=>x._id).indexOf(data.scenario))];
+                $scope.reviews.push(data);
 
+                $('#addModal').modal('toggle');
+            })
+		}
+        
+        $scope.fillmodal = function(data){
+		    $scope.editreview = angular.copy(data);
+            console.log($scope.editreview.rate);
+        }
+
+        $scope.edit = function(){
+            var temp = new Review($scope.editreview);
+            temp.$update().then((data)=>{
+                var index = $scope.reviews.map(x=>x._id).indexOf(temp._id);
+                /*data.user = $scope.users[($scope.users.map(x=>x._id).indexOf(data.user))];
+                data.scenario = $scope.scenarios[($scope.scenarios.map(x=>x._id).indexOf(data.scenario))];*/
+                $scope.reviews.splice(index,1,$scope.editreview);
+                $('#editModal').modal('toggle');
+            })
+        }
 
     }
-    ControllerFN.$inject = ['$scope','Review','Session','$state'];
+    ControllerFN.$inject = ['$scope','User','Scenario','Review','Session','$state'];
     angular.module('app').controller('ReviewsController', ControllerFN);
 })(angular);
