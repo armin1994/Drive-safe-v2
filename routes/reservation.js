1a081baf340e0b6c52c7472f25897a749667cfda
Ayoub Ben Achour <ayoub.benachour@esprit.tn>
2017-04-19 04:03:03 +0100

    Update reservation.js

--- routes/reservation.js
+++ routes/reservation.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Ctrl = require('../controllers/reservation.controller');
 router.post('/', Ctrl.add);
-router.put('/result/:id', Ctrl.updateScore);
+router.post('/result/:id', Ctrl.updateScore);
 router.put('/:id', Ctrl.update);
 router.delete('/:id', Ctrl.delete)
 router.get('/', Ctrl.getAll);
